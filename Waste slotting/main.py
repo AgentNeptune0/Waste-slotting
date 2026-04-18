0c39eec4999faa20d8fda79464c3c27572753de7
AgentNeptune0 <shanthiparent@gmail.com>
2026-04-18 11:57:55 +0100

    Update main.py

--- Waste slotting/main.py
+++ Waste slotting/main.py
@@ -11,8 +11,8 @@ WIDTH = 1000
 HEIGHT = 700
 message ="Slot the waste into the corrects bins"
 final_level = 5
-start_speed = 7
-game_over = True
+start_speed = 1
+game_over = False
 game_complete = False
 current_level = 1
 ITEMS = [("banana_peel","3"),("battery_waste","2"),("broken_ceramics","4"),("eggshells","3"),("pizza_box","4"),("plastic_bottle","1"),("soda_can","1"),("syringe","2")]
@@ -31,6 +31,7 @@ general_waste.pos = (850,600)
 bins = {"1":recycle_bin,"2":hazardous_waste_bin,"3":organic_food_bin,"4":general_waste}
 
 
+
 def create_trash_items():
     #removing old trash items
     items.clear() 
@@ -38,11 +39,11 @@ def create_trash_items():
     for i in range(current_level+1):
         #randomly picking trash item images from ITEMS
         image,bin_number = random.choice(ITEMS)
-        print(image)
+        #print(image)
         waste = Actor(image)
 
         #Setting a random x axis position
-        waste.x = random.randint(100,900)
+        waste.x = random.randint(100,WIDTH-100)
 
         #Setting y axis from the top of the window
         waste.y = 0
@@ -61,10 +62,50 @@ def draw():
     hazardous_waste_bin.draw()
     organic_food_bin.draw()
     general_waste.draw()
+    screen.draw.text("1",center = (100,600),fontsize=30,color="black")
+    screen.draw.text("2",center = (350,600),fontsize=30,color="black")
+    screen.draw.text("3",center = (600,600),fontsize=30,color="black")
+    screen.draw.text("4",center = (850,625),fontsize=30,color="black")
     if game_over == True:
         screen.draw.text("Game Over,You Lose!",center = (WIDTH//2,HEIGHT//2),fontsize=56,color="red") 
     elif game_complete == True:
         screen.draw.text("Congratulations,you win!",center = (WIDTH//2,HEIGHT//2))
+    else:
+        for waste in items:
+            waste.draw()
+    
+
+def update():
+    global game_over
+
+    #stop update if game is finished
+    if game_over == True or game_complete == True:
+        return
+    
+    #Moving each item 
+    for waste in items:
+        waste.y +=  start_speed
+        if waste.y >HEIGHT-100:
+            game_over = True
+
+def on_mouse_down(pos):
+    global current_level,game_over,game_complete
+    if game_complete == True or game_over == True:
+        return
+    if len(items) == 0:
+        return
+    
+    #Checking the first item
+    first_item = items[0]
+    
+    for key,bin_Actor in bins.items():
+        if bin_Actor.collidepoint(pos):
+
+            #Checking for the correct bin
+            
+
+
+
 
  
 
